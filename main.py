8a43456b05eff9a7593e7f2709d59826efcf3dd7
dawidg81 <dawidgorski.m@gmail.com>
2025-12-21 16:47:56 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio, socket
+
+print "simplechat v0.0.0 2025-12-21 16:17"
+
+PORT = 5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    with conn:
+        print(f"New connection incoming from {addr}")
+
+        while True:
+            clientId = conn.recv(16)
+            
