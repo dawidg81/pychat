86926287ac64cdee0552608baea58f62f6d7005f
dawidg81 <dawidgorski.m@gmail.com>
2025-12-22 17:22:41 +0100

    Changed name

--- server.py
+++ server.py
@@ -19,4 +19,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print "New connection did not send anything. Closing it!"
                 break
             
-            serverId = {"The Classical Grand", "Welcome!"}
+            serverId = {"A pychat Server", "Welcome!"}
