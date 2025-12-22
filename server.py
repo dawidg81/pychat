7886383c6aa27c4b5220e782e4f5051716c7e6d9
dawidg81 <dawidgorski.m@gmail.com>
2025-12-22 21:51:37 +0100

    Handling server parameters stability

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 import socket
 
-print "pychat server v0 2025-12-22 17:23"
+print("pychat server v0 2025-12-22 17:23")
+
+while True:
+
+    serverName = input("Name of the server? ")
+    
+    if len(serverName) > 16:
+        print("Name of the server can not be longer than 16 characters!")
+
+    serverMOTD = input("Message Of The Day? ")
+
+    if len(serverMOTD) > 64:
+        print("Message Of The Day can not be longer than 64 characters!")
 
 PORT = input("On what port number this server will listen? ")
 
@@ -19,4 +31,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print "New connection did not send anything. Closing it!"
                 break
             
-            serverId = {"A pychat Server", "Welcome!"}
+            serverId = {serverName, serverMOTD}
